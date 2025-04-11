64ef1d3323b95d1d651e227e622a53fadfc3ab5d
blockfunavry <nasir.ali@funavry.com>
2025-04-11 10:15:45 +0500

    update .env.test

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,12 +1,11 @@
 import { mainnet, arbitrum, sepolia ,bsc,bscTestnet, base, baseSepolia, sonic, sonicTestnet, polygon, avalanche, zksync, coreDao, xLayer, linea, blast, berachain, polygonAmoy, solana, optimism} from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
-import { core } from 'web3'
 import { SolanaAdapter } from '@reown/appkit-adapter-solana'
 // import { mainnet, arbitrum, sepolia, bsc } from '@reown/appkit' // adjust import path as needed
 
 // Get projectId from https://cloud.reown.com
-export const projectId = import.meta.env.VITE_PROJECT_ID || "1e8fbe566ab223dac6fd8f4e0fdbf82c" // this is a public projectId only to use on localhost
+export const projectId = import.meta.env.VITE_PROJECT_ID // this is a public projectId only to use on localhost
 console.log("Current projectId", projectId)
 if (!projectId) {
   throw new Error('Project ID is not defined')
